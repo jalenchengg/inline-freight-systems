2f6a20b2b8d1bf00ae52149f38ef3d57d554e650
Joe Lee <lefodev@gmail.com>
2025-06-19 00:11:55 -0700

    Add partners

---
+++ lib/partners.ts
@@ -0,0 +1,46 @@
+export const partners = [
+  {
+    url: "https://www.msc.com/",
+    logo: "/partners/msc.png",
+  },
+  {
+    url: "https://www.cma-cgm.com/",
+    logo: "/partners/cmacgm.png",
+  },
+  {
+    url: "https://www.maersk.com/",
+    logo: "/partners/maersk.png",
+  },
+  {
+    url: "https://www.yangming.com/en",
+    logo: "/partners/yangming.png",
+  },
+  {
+    url: "https://lines.coscoshipping.com/",
+    logo: "/partners/cosco.png",
+  },
+  {
+    url: "https://www.evergreen-marine.com/",
+    logo: "/partners/evergreen.png",
+  },
+  {
+    url: "https://www.oocl.com/",
+    logo: "/partners/oocl.png",
+  },
+  {
+    url: "https://us.one-line.com/",
+    logo: "/partners/one.png",
+  },
+  {
+    url: "https://www.hmm21.com/company.do",
+    logo: "/partners/hmm.png",
+  },
+  {
+    url: "https://www.zim.com/",
+    logo: "/partners/zim.png",
+  },
+  {
+    url: "https://www.hapag-lloyd.com/",
+    logo: "/partners/hapaglloyd.png",
+  },
+];
